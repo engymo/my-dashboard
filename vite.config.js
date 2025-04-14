95238bf50c4bae051e84cbd6976c8d3f00f38054
angie <noga23121993@gmail.com>
2025-04-14 20:21:31 +0200

    upload source code

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-dashboard/', // لازم تضيف اسم الريبو هنا
+
 })
